6b9ee54f125cea608f7d2d3256255b0174b2e82e
KamilBedlin <kamcio44@gmail.com>
2022-06-10 20:41:07 +0200

    Schody dopracowane

--- BridgeRacer/Assets/Stairs/StepInstantiating.cs
+++ BridgeRacer/Assets/Stairs/StepInstantiating.cs
@@ -4,26 +4,57 @@ using UnityEngine;
 
 public class StepInstantiating : MonoBehaviour
 {
-    public GameObject step;
-    public List<GameObject> steps;
+    public GameObject step; //Step prefab
+    public List<GameObject> steps = new List<GameObject>(); //List of all steps in stairs
 
-    int iter = 0;
-    readonly Vector2 stepEdge = new Vector2(0.173205f, 0.3f);
+    int iter = 0; //Last step
+    readonly Vector2 stepSize = new Vector2(0.173205f, 0.3f);
 
     private void OnCollisionStay(Collision collision)
     {
+        //Position of the next step that will be added to the stairs
         Vector3 newStep = new Vector3(this.transform.position.x,
-            iter * stepEdge.x - stepEdge.x / 2,
-            this.transform.position.z + iter * stepEdge.y + stepEdge.y / 2);
+            iter * stepSize.x - stepSize.x / 2,
+            this.transform.position.z + iter * stepSize.y + stepSize.y / 2);
 
-        if (collision.transform.position.z > newStep.z - stepEdge.y / 2 &&
-            collision.transform.position.z < newStep.z + stepEdge.y / 2)
+        if (collision.transform.position.z > newStep.z - stepSize.y / 2 &&
+            collision.transform.position.z < newStep.z + stepSize.y / 2)
         {
+            //Creates a step, sets it's color and adds to the list
             GameObject stp = Instantiate(step, newStep, Quaternion.identity);
-            stp.GetComponent<Renderer>().material = collision.gameObject.GetComponent<Renderer>().material;
+            stp.transform.parent = this.transform;
+            stp.GetComponent<Renderer>().material.color = collision.gameObject.GetComponent<Renderer>().material.color;
             steps.Add(stp);
             iter++;
         }
+        else //If the character of different color gets on already created step change that step's color
+        {
+            
+            GameObject stepBelow = FindTheStepBelow(collision);
+            if (stepBelow != null)
+            {
+                var stepMaterial = stepBelow.GetComponent<Renderer>().material;
+                var collisionMaterial = collision.gameObject.GetComponent<Renderer>().material;
+                if (stepMaterial.color != collisionMaterial.color)
+                    stepMaterial.color = collisionMaterial.color;
+            }
+        }
 
+    }
+
+    private GameObject FindTheStepBelow(Collision collision) //Finds the step directly below the character
+    {
+        foreach (var step in steps)
+        {
+            Vector2 stepBorders = new Vector2(step.transform.position.z - stepSize.y / 2,
+                step.transform.position.z + stepSize.y / 2);
+            if (collision.transform.position.z > stepBorders.x &&
+                collision.transform.position.z < stepBorders.y)
+            {
+                return step; 
+            }
+                
+        }
+        return null;
     }
 }
